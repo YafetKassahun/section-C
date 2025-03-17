ef60e3b977ba691e96a7b8b07fdbebaa15ae8a55
Yafet Kassahun <yafetalazar0104@gmail.com>
2025-03-17 23:29:50 +0300

    Add files via upload

---
+++ str_isupper.py
@@ -0,0 +1,5 @@
+data = "HI MY NAME IS YAFET KASSAHUN AND I'M A THIRD YEAR ELECTRO MECHANICAL STUDENT."
+
+isupper = data.isupper()
+print(isupper) # from this when we run the code we're expecting the outcome to be true because of the written data is in uppercase, otherwise it is false.
+
