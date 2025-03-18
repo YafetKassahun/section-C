4c0e8d5c73b1688a701fe03b71110070f0b1a96e
Yafet Kassahun <yafetalazar0104@gmail.com>
2025-03-19 00:49:41 +0300

    Add files via upload

---
+++ islower.py
@@ -0,0 +1,5 @@
+data = "hi my name is yafet kassahun and i'm a third year electro mechanical student."
+
+islower = data.islower()
+print(islower) # from this when we run the code we're expecting the outcome to be true because of the written data is in lowercase, otherwise it is false.
+
